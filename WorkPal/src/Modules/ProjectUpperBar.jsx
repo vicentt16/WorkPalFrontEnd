ac8029899f94795d23ceec52bba4ecc215c60b0e
vicentt16 <vicentecvarguez@gmail.com>
2026-03-12 22:06:15 -0600

    adding module ProjectUpperBar

---
+++ WorkPal/src/Modules/ProjectUpperBar.jsx
@@ -0,0 +1,17 @@
+
+
+export function ProjectUpperBar() {
+    return(
+        <div>
+            <img src="" alt="WorkPal Logo" />
+
+            <button>
+                <img src="" alt="Search Icon" />
+            </button>
+
+            <button>
+                <img src="" alt="Create Icon" />
+            </button>
+        </div>
+    )
+}
