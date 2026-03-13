70478aafe4574670005c81a2e0f8f1f77ac7c07f
vicentt16 <vicentecvarguez@gmail.com>
2026-03-12 22:13:41 -0600

    Basic Layout for UpperBar

--- WorkPal/src/Modules/ProjectUpperBar.jsx
+++ WorkPal/src/Modules/ProjectUpperBar.jsx
@@ -1,17 +1,24 @@
-
-
 export function ProjectUpperBar() {
-    return(
-        <div>
-            <img src="" alt="WorkPal Logo" />
+  return (
+    <div>
+      <img src="" alt="WorkPal Logo" />
 
-            <button>
-                <img src="" alt="Search Icon" />
-            </button>
+      <section>
+        <button>
+          <img src="" alt="Search Icon" />
+          <h2>Buscar</h2>
+        </button>
 
-            <button>
-                <img src="" alt="Create Icon" />
-            </button>
-        </div>
-    )
+        <button>
+          <img src="" alt="Create Icon" />
+          <h2>Crear Proyecto</h2>
+        </button>
+
+        <button>
+          <img src="" alt="Register Icon" />
+          <h2>Registrarse</h2>
+        </button>
+      </section>
+    </div>
+  );
 }
