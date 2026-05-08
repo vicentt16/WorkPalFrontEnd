71a640e236022982a79bc56229580428e565a4d4
vicentt16 <vicentecvarguez@gmail.com>
2026-05-08 12:48:55 -0600

    initial comit
    created the file and function  buscarProyectos.jsx

---
+++ WorkPal/src/Modules/BucarProyecto.jsx
@@ -0,0 +1,10 @@
+
+
+export function BuscarProyecto() {
+    return (
+        <div>
+            <h1>Buscar Proyecto</h1>
+            {/* Aquí puedes agregar el formulario o la lógica para buscar proyectos */}
+        </div>
+    );
+}
