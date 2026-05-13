3c9d5b1e7819aed68b0f3c6158dfe9812b4fb3c6
AngelRosadoWTF <angsoft05@gmail.com>
2026-05-13 00:52:03 -0600

    Organización de archivos
    Organización de archivos.

--- WorkPal/src/Pages/Register/RegisterUser.jsx
+++ WorkPal/src/Pages/Register/RegisterUser.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ProjectUpperBar } from "../Modules/ProjectUpperBar";
+import { ProjectUpperBar } from "../../Modules/ProjectUpperBar";
 import "./RegisterUser.css";
 
 export default function RegisterUser() {
