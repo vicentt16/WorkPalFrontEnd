4e84a89d2dbdc006b1f2728c3d4745bb7fe4ba1e
vicentt16 <vicentecvarguez@gmail.com>
2026-05-12 18:16:08 -0600

    dd

--- WorkPal/src/Modules/BucarProyecto.jsx
+++ WorkPal/src/Modules/BucarProyecto.jsx
@@ -4,7 +4,7 @@ export function BuscarProyecto() {
     return (
         <div>
             <h1>Buscar Proyecto</h1>
-            {/* Aquí puedes agregar el formulario o la lógica para buscar proyectos */}
+            
         </div>
     );
 }
--- WorkPal/src/main.jsx
+++ WorkPal/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BuscarProyecto } from './Modules/BucarProyecto.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BuscarProyecto/>
   </StrictMode>,
 )
