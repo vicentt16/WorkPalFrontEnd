16f6ffa6bd1eb21ab04b0b32821e56eb138a99da
vicentt16 <vicentecvarguez@gmail.com>
2026-05-13 11:04:03 -0600

    Revert "Merge branch 'BuscarProyecto' of https://github.com/vicentt16/WorkPalFrontEnd into BuscarProyecto"
    This reverts commit bacbcb293a090b88c12a3c6b905c0f03b5387aa3, reversing
    changes made to 4e84a89d2dbdc006b1f2728c3d4745bb7fe4ba1e.

--- WorkPal/src/main.jsx
+++ WorkPal/src/main.jsx
@@ -1,15 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BuscarProyecto } from './Modules/BucarProyecto.jsx'
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AuthProvider } from "./Context/AuthContext";
-
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BuscarProyecto/>
+  </StrictMode>,
+)
