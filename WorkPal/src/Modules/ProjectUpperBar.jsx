c18eb5e1e7577208361d3580215025545cc3ceb6
AngelRosadoWTF <angsoft05@gmail.com>
2026-05-21 13:16:15 -0600

    Cambios
    - Estilo de Crear Proyecto

--- WorkPal/src/Modules/ProjectUpperBar.jsx
+++ WorkPal/src/Modules/ProjectUpperBar.jsx
@@ -1,32 +1,81 @@
+import "./ProjectUpperBar.css";
+
 import { useNavigate } from "react-router-dom";
+
+import logoWorkPal from "/src/Images/logoWorkPal.png";
+import searchIcon from "/src/Images/searchIcon.png";
+import createIcon from "/src/Images/createIcon.png";
+import registerIcon from "/src/Images/registerIcon.png";
 
 export function ProjectUpperBar() {
   const navigate = useNavigate();
 
   return (
-    <header className="upper-bar">
-      <img
-        src={logoWorkPal}
-        alt="logo"
-        className="logoWorkPal"
-        onClick={() => navigate("/home")}
-      />
+    <header className="upper-bar-header">
+      <div className="upper-bar">
+        {/* LOGO */}
+        <img
+          src={logoWorkPal}
+          alt="WorkPal Logo"
+          className="logoWorkPal"
+          onClick={() => navigate("/home")}
+          style={{ cursor: "pointer" }}
+        />
 
-      <div className="upper-bar-buttons">
-        <button onClick={() => navigate("/search")}>
-          <img src={searchIcon} alt="search" />
-          <h2>Buscar</h2>
-        </button>
+        {/* NAV BUTTONS */}
+        <div className="upper-bar-buttons">
+          <button
+            className="upper-bar-button"
+            onClick={() => navigate("/search")}
+            type="button"
+          >
+            <img
+              src={searchIcon}
+              alt="Search Icon"
+              className="upper-bar-icon"
+            />
 
-        <button onClick={() => navigate("/create-project")}>
-          <img src={createIcon} alt="create" />
-          <h2>Crear Proyecto</h2>
-        </button>
+            <h2 className="upper-bar-title">
+              Buscar
+            </h2>
+          </button>
 
-        <button onClick={() => navigate("/")}>
-          <img src={registerIcon} alt="login" />
-          <h2>Login</h2>
-        </button>
+          <button
+            className="upper-bar-button"
+            onClick={() =>
+              navigate("/create-project")
+            }
+            type="button"
+          >
+            <img
+              src={createIcon}
+              alt="Create Icon"
+              className="upper-bar-icon"
+            />
+
+            <h2 className="upper-bar-title">
+              Crear Proyecto
+            </h2>
+          </button>
+
+          <button
+            className="upper-bar-button register-button"
+            onClick={() =>
+              navigate("/register")
+            }
+            type="button"
+          >
+            <img
+              src={registerIcon}
+              alt="Register Icon"
+              className="upper-bar-icon"
+            />
+
+            <h2 className="upper-bar-title">
+              Registrarse
+            </h2>
+          </button>
+        </div>
       </div>
     </header>
   );
